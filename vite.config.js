355c9f8952e04adfab57094f9e2d098894de9fe6
Bennyho2015 <125178782+Bennyho2015@users.noreply.github.com>
2026-03-29 02:43:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+build: { outDir: 'build', target: 'esnext' }
+完整範例（整個檔案）
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    target: 'esnext'
   }
 });
